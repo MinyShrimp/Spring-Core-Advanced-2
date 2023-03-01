7f2373141a7ff37afa85ce3fcca0f98c3005b334
MinyShrimp <ksk7584@gmail.com>
2023-03-01 12:13:08 +0900

    프로젝트 생성

---
+++ src/main/java/hello/springcoreadvanced2/ProxyApplication.java
@@ -0,0 +1,13 @@
+package hello.springcoreadvanced2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/springcoreadvanced2/SpringCoreAdvanced2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcoreadvanced2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAdvanced2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
